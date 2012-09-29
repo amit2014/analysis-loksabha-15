1d6cc811465e9eb959a7938bf77f999e0c1ebc19
Shivanker Goel <shivanker.goel@gmail.com>
2012-09-29 11:21:07 +0530

    nothing much, pie samajh nhi aayi

--- pie.php
+++ pie.php
@@ -4,17 +4,133 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="css/ui-lightness/jquery-ui-1.8.23.custom.css">
 <script type="text/javascript" src="js/d3.v2.js"></script>
-<script type="text/javascript" src="js/pie.js"></script>
-<script>
-draw_pie(<?php 
-			if(isset($_GET['field'])){
-				echo "\"";
-				echo urldecode($_GET['field']) ;
-				echo "\"";
+<script type="text/javascript">
+
+<?php
+function getvar($vname, $deflt)  { 
+  if(isset($_GET[$vname])){
+    echo "\"";
+    echo urldecode($_GET[$vname]) ;
+    echo "\"";
+  }
+  else
+  echo "'$deflt'"; 
+}
+?>
+
+	var field = <?php 
+			getvar('field','Political party');
+			?>;
+	var data = [];
+	d3.csv("MPTrack.csv", function(mps) {
+		var fieldd = [];
+
+		for (var j = mps.length - 1; j >= 0; j--) {
+			var key = mps[j][field];
+			if (fieldd.indexOf(key) >= 0) {
+				data[fieldd.indexOf(key)]++;
 			}
-			else
-			echo "'Age'"; 
-			?>);
+			else {
+				fieldd.push(key);
+				data.push(1);
+			}
+		}
+		/*for (var i = mps.length - 1; i >= 0; i--) {
+		 if (mps[i]["Age"]>70)
+		 data[0]++;
+		 else if (mps[i]["Age"]>50)
+		 data[1]++;
+		 else if (mps[i]["Age"]>30)
+		 data[2]++;
+		 else
+		 data[3]++;
+		 }*/
+		console.log(data);
+		console.log(fieldd);
+		/*var colors = [];
+		 var color = Math.random()*360, off = 4*360/39;
+		 for(var i = 1; i <= 50; ++i)
+		 colors.push( "hsl(" + ((color+off*i)%360) + ", 60%, 60%)");
+		 var z = d3.scale.ordinal()
+		 .range(colors);*/
+		var width = 400,
+			height = 400,
+			outerRadius = Math.min(width, height) / 2,
+			innerRadius = outerRadius * .6,
+			radius = Math.min(width, height) / 2,
+			data2 = d3.range(10).map(Math.random).sort(d3.descending),
+			color = d3.scale.category20c(),
+			arc = d3.svg.arc().outerRadius(radius),
+			arc2 = d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius);
+		donut = d3.layout.pie();
+
+
+		var vis = d3.select("div").append("svg").data([data]).attr("width", width).attr("height", height);
+
+		var arcs = vis.selectAll("g.arc").data(donut).enter().append("g").attr("class", "arc").attr("transform", "translate(" + radius + "," + radius + ")");
+
+		var paths = arcs.append("path").attr("style","stroke:#000; stroke-width:0.2")
+			.attr("fill", function(d, i) {
+				return color(i);
+			}).on("mouseover", function(d, i) {
+				vis.selectAll("path").filter(function(d, j) {
+					return i == j;
+				}).style("fill", function(d, j) {
+					return d3.rgb(color(i)).brighter();
+				});
+			}).on("mouseout", function(d, i) {
+				vis.selectAll("path").style("fill", function(d, i) {
+					return color(i);
+				});
+			});
+	
+		paths.append("title").text(function(d,i){return fieldd[i]+': '+(data[i]*100/mps.length).toFixed(2)+'%'});
+		
+
+
+		/*arcs.append("text")
+		 .attr("transform", function(d) { return "translate(" + arc2.centroid(d) + ")"; })
+		 .attr("dy", ".35em")
+		 .attr("text-anchor", "middle")
+		 .attr("display", function(d) { return d.value > .15 ? null : "none"; })
+		 .text(function(d, i) { 
+		 return fieldd[i];
+		 });*/
+		//d3.select("body").append("br");
+/*		d3.select("body").selectAll("span").data(fieldd).enter().append("span").attr("style", function(d, i) {
+				return " font-size:12px; " + "color:" + color(i)
+			}).html(function(d, i) {
+				return d + ':' + data[i] + '<br/>'
+			});*/
+
+
+		paths.transition().ease("bounce").duration(1000).attrTween("d", tweenPie);
+
+		paths.transition().ease("bounce").delay(function(d, i) {
+				return 2000 - i * 20;
+			}).duration(500).attrTween("d", tweenDonut);
+
+		function tweenPie(b) {
+				b.innerRadius = 0;
+				var i = d3.interpolate({
+					startAngle: 0,
+					endAngle: 0
+				}, b);
+				return function(t) {
+					return arc(i(t));
+				};
+			}
+
+		function tweenDonut(b) {
+				b.innerRadius = radius * 0.1;
+				var i = d3.interpolate({
+					innerRadius: 0
+				}, b);
+				return function(t) {
+					return arc(i(t));
+				};
+			}
+	});
 </script>
 <title>Pie</title>
 </head>
